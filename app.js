88fc1f5b8f1d554187a5824c755c973763a96e5b
Hrithik MJ <31767239+HrithikMJ@users.noreply.github.com>
2022-12-10 21:04:03 +0530

    fix

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const functions = require('firebase-functions');
+// const functions = require('firebase-functions');
 const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
