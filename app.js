b30dfdd85b5af8dce96926f203b59a13d89320d3
Dharundds <dharundds@gmail.com>
2022-12-10 11:40:18 +0530

    server

--- app.js
+++ app.js
@@ -1,181 +1,155 @@
 const functions = require('firebase-functions');
-const cors = require("cors");
-const express = require("express");
-const bodyParser = require("body-parser");
-const fs = require("fs");
-const app = express();
-var nodemailer = require("nodemailer");
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+const fs = require('fs')
+const cors = require('cors')({origin: true});
+admin.initializeApp();
 
 // var sender = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: "office@learningroomdigital.com",
-//     pass: "nsminxxrfxrjlxpc",
-//   },
-// });
+//   service:"gmail",
+//   auth:{
+//     user:"dharundds@gmail.com",
+//     pass:"becjaziwymugppqv"
+//   }
+// })
 
-var sender = nodemailer.createTransport({
-  service:"gmail",
-  auth:{
-    user:"dharundds@gmail.com",
-    pass:"becjaziwymugppqv"
-  }
-})
-
-const multer = require("multer");
-const storage = multer.diskStorage({
-  destination: (req, file, callBack) => {
-    callBack(null, "uploads");
-  },
-  filename: (req, file, callBack) => {
-    callBack(null, `${file.originalname}`);
-  },
-});
-let upload = multer({ dest: "uploads/" });
-let uploadReceipt = multer({ dest: "uploadsReceipt/" });
-
-app.use(
-  cors({
-    origin: ["https://learningroomdigital.com/form/","http://localhost:3000"],
-    //origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    optionsSuccessStatus: 200,
-  })
-);
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
-app.post("/learningroom/contact",(req,res)=>{
-  const fname = req.body.fname;
-  const email = req.body.email;
-  const phonenum = req.body.phonenum;
-  const message = req.body.message;
-
-  var mail = {
-    from:"dharundds@gmail.com",
-    to:["dharundds@gmail.com"],
-    subject: `Enquiry submitted from Learningroom website by ${fname}`,
-    html:` 
-      <style>
-          span{
-            font-weight: bold;
-          }
-      </style>
-      <h3>Respect Sir,</h3>
-      <p>An enquiry is submitted by ${fname} and phone number: <span>${phonenum}</span>, email: <span>${email}</span> </p>
-      <p>The message sent is : <span>${message}</span></p>`
-  }
-
-  sender.sendMail(mail,(error,info)=>{
-    if(error){
-      console.log(error);
-      res.send(error);
-    }else{
-      console.log("Email sent successfully: ", info.response);
-      res.send(info.accepted)
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'dharundds@gmail.com',
+        pass: 'becjaziwymugppqv'
     }
-  })
+});
 
-})
+exports.contact = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+        const body = JSON.parse(req.body)
+        // getting dest email by query string
+        const fname = body.fname;
+        const email = body.email;
+        const phonenum = body.phonenum;
+        const message = body.message;
 
-app.post("/uploadFileAPI", upload.single("file"), (req, res, next) => {
-  const file = req.file;
-  const sname = req.body.sname;
-  const sclass= req.body.sclass;
+        const mailOptions = {
+            from:"dharundds@gmail.com",
+            to:["dharundds@gmail.com","hrithimj003@gmail.com"],
+            subject: `Enquiry submitted from Learningroom website by ${fname}`,
+            html:` 
+              <style>
+                  span{
+                    font-weight: bold;
+                  } 
+              </style>
+              <h3>Respect Sir,</h3>
+              <p>An enquiry is submitted by ${fname} and phone number: <span>${phonenum}</span>, email: <span>${email}</span> </p>
+              <p>The message sent is : <span>${message}</span></p>`
+        };
   
-  if (!file) {
-    const error = new Error("No File");
-    error.httpStatusCode = 400;
-    return next(error);
-  }
-  res.send(file);
-  var a = file.originalname.split(".");
-  let fileName = a[0] + "_" + file.filename + "." + a[1];
-  fs.rename("uploads/" + file.filename, "uploads/" + fileName, () => {});
-  var mail = {
-    // from: "office@learningroomdigital.com",
-    // to: ["office@learningroomdigital.com","class1@learningroomdigital.com"],
-    from:"dharundds@gmail.com",
-    to:["dharundds@gmail.com"],
-    subject: `Application for ${sname}`,
-    text: `The application form of the student ${sname} of class ${sclass} is attached below`,
-    attachments: [
-      {
-        filename: `${fileName}`,
-        path: `uploads/${fileName}`,
-      },
-    ],
-  };
-
-  sender.sendMail(mail, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent successfully: " + info.response);
-      let file = "uploads/" + fileName;
-      fs.unlinkSync(file);
-    }
-  });
+        // returning result
+        return transporter.sendMail(mailOptions, (erro, info) => {
+            if(erro){
+                return res.send(erro.toString());
+            }
+            return res.send('Sended');
+        });
+    });    
 });
 
-app.post(
-  "/uploadFileAPIReceipt",
-  uploadReceipt.single("file"),
-  (req, res, next) => {
-    const file = req.file;
-    const sname = req.body.sname;
-    const sclass = req.body.sclass;
-    const ino = req.body.ino;
-    let g;
 
-    if (ino==1){
-      g='st'
-    }else if (ino==2){
-      g='nd'
-    }else{
-      g='th'
-    }
-    if (!file) {
-      const error = new Error("No File");
-      error.httpStatusCode = 400;
-      return next(error);
-    }
-    res.send(file);
-    var a = file.originalname.split(".");
-    let fileName = a[0] + "_" + file.filename + "." + a[1];
-    fs.rename(
-      "uploadsReceipt/" + file.filename,
-      "uploadsReceipt/" + fileName,
-      () => {}
-    );
-    var mail = {
-      // from: "office@learningroomdigital.com",
-      // to: ["office@learningroomdigital.com","class1@learningroomdigital.com"],
-      from:"dharundds@gmail.com",
-    to:["dharundds@gmail.com"],
-      subject: `Receipt for ${sname}`,
-      text: `The student ${sname} of class ${sclass} has paid the ${ino}${g} installation. The receipt is attached below`,
-      attachments: [
-        {
-            filename: `${fileName}`,
-            path: `uploadsReceipt/${fileName}`,
+
+exports.application = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+      
+        // getting dest email by query string
+        const file = req.file;
+        const sname = req.body.sname;
+        const sclass= req.body.sclass;
+        
+        if (!file) {
+            const error = new Error("No File");
+            error.httpStatusCode = 400;
+            return next(error);
         }
-    ]
-    };
+        res.send(file);
+        var a = file.originalname.split(".");
+        let fileName = a[0] + "_" + file.filename + "." + a[1];
+        fs.rename("uploads/" + file.filename, "uploads/" + fileName, () => {});
 
-    sender.sendMail(mail, function(error, info) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Email sent successfully: "
-                     + info.response);
-        let file = 'uploadsReceipt/'+fileName;
-        fs.unlinkSync(file)
-      }
-    });
-   
-  }
-);
+        const mailOptions = {
+            from:"dharundds@gmail.com",
+            to:["dharundds@gmail.com","hrithimj003@gmail.com"],
+            subject: `Enquiry submitted from Learningroom website by ${fname}`,
+            text: `The application form of the student ${sname} of class ${sclass} is attached below`,
+            attachments: [
+            {
+                filename: `${fileName}`,
+                path: `uploads/${fileName}`,
+            },
+    ],
+        };
+  
+        // returning result
+        return transporter.sendMail(mailOptions, (erro, info) => {
+            if(erro){
+                return res.send(erro.toString());
+            }
+            return fs.unlinkSync("uploads/" + fileName);
+        });
+    });    
+});
 
-exports.email = functions.https.onRequest(app);
+
+
+exports.receipt = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+      
+        // getting dest email by query string
+        const file = req.file;
+        const sname = req.body.sname;
+        const sclass = req.body.sclass;
+        const ino = req.body.ino;
+        let g;
+
+        if (ino==1){
+        g='st'
+        }else if (ino==2){
+        g='nd'
+        }else{
+        g='th'
+        }
+        if (!file) {
+        const error = new Error("No File");
+        error.httpStatusCode = 400;
+        return next(error);
+        }
+        res.send(file);
+        var a = file.originalname.split(".");
+        let fileName = a[0] + "_" + file.filename + "." + a[1];
+        fs.rename(
+        "uploadsReceipt/" + file.filename,
+        "uploadsReceipt/" + fileName,
+        () => {}
+        );
+        const mailOptions = {
+            from:"dharundds@gmail.com",
+            to:["dharundds@gmail.com","hrithimj003@gmail.com"],
+            subject: `Enquiry submitted from Learningroom website by ${fname}`,
+            subject: `Receipt for ${sname}`,
+            text: `The student ${sname} of class ${sclass} has paid the ${ino}${g} installation. The receipt is attached below`,
+            attachments: [
+              {
+                  filename: `${fileName}`,
+                  path: `uploadsReceipt/${fileName}`,
+              }
+          ]
+        };
+  
+        // returning result
+        return transporter.sendMail(mailOptions, (erro, info) => {
+            if(erro){
+                return res.send(erro.toString());
+            }
+            return fs.unlinkSync('uploadsReceipt/'+fileName);
+        });
+    });    
+});
